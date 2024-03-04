6a2abf0ae2fa68e6454d24f67c4bdbc02e9066ac
Mohammed <52212449+Mohammed-ela@users.noreply.github.com>
2024-03-04 17:53:05 +0100

    Delete recipes directory

--- recipes/recipes.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[recipes]
-(
-    id INT PRIMARY KEY IDENTITY(1,1),
-    created_at DATETIME DEFAULT GETDATE(),
-    name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    people INT NOT NULL,
-    preparation_time INT NOT NULL
-);
